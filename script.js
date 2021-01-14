e443c622ece23dbfc9d09c8cbbf8b50dddfa7dba
Daniel <daniel.castelline@gmail.com>
2021-01-13 20:44:38 -0600

    fixed js file link, list of possible var in js, ready and click functions made

---
+++ script.js
@@ -0,0 +1,25 @@
+// // Possible needed variables
+//     // search input
+//     var searches = "";
+//     // saved cities
+//     var returns = "";
+//     // current city display
+//     var currCity = "";
+//     // 5 day forecast
+//     var fiveDay = "";
+//     // date
+//     var currDate = "";
+    // // submit button
+    // var searchBtn = "";
+    // // URL for AJAX
+    // var queryURL = "";
+
+$(document).ready(function() {
+    console.log("ready");
+  
+    $("#searchBtn").on("click", function(event) {
+        event.preventDefault();
+        console.log("search");
+       
+    })
+})
