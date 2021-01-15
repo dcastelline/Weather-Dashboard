19de3bede2fd4f21ec2d9eb3e9d9408dc809a1e6
Daniel <daniel.castelline@gmail.com>
2021-01-14 20:04:18 -0600

    rearranged js functions and variables, displaying city list, function to getItems, weather function

--- script.js
+++ script.js
@@ -10,28 +10,73 @@
     // // URL for AJAX
     // var queryURL = "";
 
-// saved cities
-var returns = [];
+// API key
+var apiKey = "0f0bc1c3fcb1960a5ade4fdb7f4a77ef"
 
+// Saved cities searched
+var returns = [];
+var searchedCities = $(".searched-cities");
+
+// Ready function
 $(document).ready(function() {
     console.log("ready");
-  
+    
+    //Display searched cities
+    function searchedCities() {
+        returns.empty();
+        $("#search-field").val("");
+        for (i = 0; i < returns.length; i++) {
+            var a = $("<a>");
+            a.addClass("searched-city");
+            a.text(returns[i]);
+            returns.prepend(a);
+
+        }
+    };
+
+    // Pull Cities from local storage
+    function pullCities() {
+        var returnedCities = JSON.parse(localStorage.getItem("cities"));
+
+        if (returnedCities !== null) {
+            returns = returnedCities;
+        }
+        returnedCities;
+    };
+
+    // Use the input to get weather
+    function checkWeather() {
+        var checkedWeather = JSON.parse(localStorage.getItem("currCity"));
+
+        if (checkWeather !== null) {
+           searchedCity = checkWeather;
+           showWeather();
+           showForecast(); 
+        }
+    };
+
+    // Submit event
     $("form").submit(function(event) {
         event.preventDefault();
-        console.log("search");
-       
-        var searchInput = $(".search-field").val();
+        var searchInput = $(".search-field").val().trim();
+        console.log(searchInput);
+        
         if (searchInput === "") {
             alert("Oops! Please enter a city name to check the weather.")
-        } else if (returns.length <= 5) {
-            returns.shift();
-            returns.push(searchInput);
-            console.log(searchInput);
+            } else if (returns.length <= 5) {
+                returns.push(searchInput);
+                console.log(returns);
+            } else if (returns.length > 5) {
+                returns.slice();
+                returns.push(searchInput);
             };
+
+            // Store city
+            localStorage.setItem("city", JSON.stringify(searchInput));
+            console.log(localStorage);
         
-            // $.ajax(
-                
-            // )
+            //Check weather function
+            
         });
 
     });
